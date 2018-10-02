09e6a631a6497f2ad9c65eaaadb2863dfb36a461
Nader Abolfazli <abolfazli.nader@gmail.com>
2018-10-02 16:17:34 +0330

    fix(bot): change Location filter matcher

--- balebot/filters/location_filter.py
+++ balebot/filters/location_filter.py
@@ -1,12 +1,7 @@
-from balebot.models.messages.location_message import LocationMessage
-from balebot.models.messages.json_message import JsonMessage
 from balebot.filters.filter import Filter
+from balebot.models.messages.location_message import LocationMessage
 
 
 class LocationFilter(Filter):
     def match(self, message):
-        if isinstance(message, JsonMessage):
-            raw_json = message.raw_json
-            return isinstance(raw_json, LocationMessage)
-        else:
-            return False
+        return isinstance(message, LocationMessage)
