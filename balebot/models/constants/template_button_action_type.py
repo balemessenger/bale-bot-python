674db36e49fffc8d75d9c8f8c64e6720d080966a
Nader Abolfazli <abolfazli.nader@gmail.com>
2018-10-03 15:08:13 +0330

    feat(bot): added ButtonAction type for template messages button action

---
+++ balebot/models/constants/template_button_action_type.py
@@ -0,0 +1,10 @@
+class ButtonAction:
+    default = 1
+    browse_user = 21
+    barcode_reader = 22
+    location = 23
+    contact = 24
+    __date = 25
+    __time = 26
+    photo = 27
+    file = 28
