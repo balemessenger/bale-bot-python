2e122af284b380b868bb80590e4c08bfcb731b90
Nader Abolfazli <abolfazli.nader@gmail.com>
2018-10-03 13:10:05 +0330

    feat(bot): add QuotedMessageHandler so replied messages could handle and filter through this handler

--- balebot/handlers/__init__.py
+++ balebot/handlers/__init__.py
@@ -1,3 +1,4 @@
 from balebot.handlers.handler import Handler
 from balebot.handlers.command_handler import CommandHandler
 from balebot.handlers.message_handler import MessageHandler
+from balebot.handlers.quoted_message_handler import QuotedMessageHandler
