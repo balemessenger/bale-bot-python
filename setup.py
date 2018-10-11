ee93af41c16ba5eb1741deab5fc0418174e2efa2
Bahram Aghaei <aghaee.bahram@gmail.com>
2018-10-11 14:04:02 +0330

    separate version and other datas from setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-import setuptools
+from setuptools import setup, find_packages
+import os 
 
-with open('CHANGELOG.md', mode='rt', encoding='utf-8') as f:
-    long_description = f.read()
+here = os.path.abspath(os.path.dirname(__file__))
 
 requires = [
         'aiohttp==2.3.7',
@@ -11,14 +11,23 @@ requires = [
         'pillow==5.2.0',
         ]
 
-setuptools.setup(name='balebot',
-                 version='1.2.8',
-                 description='python framework for Bale messenger Bot API',
-                 author='bale',
-                 author_email='balebot@elenoon.ir',
-                 license='GNU',
-                 long_description=long_description,
-                 long_description_content_type='text/markdown',
-                 url='https://github.com/balemessenger/bale-bot-python',
-                 install_requires=requires,
-                 packages=setuptools.find_packages())
+with open('CHANGELOG.md', mode='rt', encoding='utf-8') as f:
+    long_description = f.read()
+
+about = {}
+with open(os.path.join(here, 'balebot', '__version__.py'), mode='rt', encoding='utf-8') as f:
+    exec(f.read(), about)
+
+setup(
+        name=about['__title__'],
+        version=about['__version__'],
+        description=about['__description__'],
+        author=about['__author__'],
+        author_email=about['__author_email__'],
+        license=about['__license__'],
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url=about['__url__'],
+        install_requires=requires,
+        packages=find_packages(),
+)
